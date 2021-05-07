de83e1d0762c3237bd62d80d8f4bad43ac5fa001
junedayday <junedayday@gmail.com>
2021-05-07 21:48:43 +0800

    add aliyun domain

---
+++ internal/domainer/domainer.go
@@ -0,0 +1,39 @@
+package domainer
+
+import (
+	"context"
+
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+
+	"github.com/cloud-fitter/cloud-fitter/gen/idl/pbdomain"
+	"github.com/cloud-fitter/cloud-fitter/gen/idl/pbtenant"
+	"github.com/cloud-fitter/cloud-fitter/internal/tenanter"
+)
+
+var (
+	ErrDomainListNotSupported = errors.New("cloud not supported domain list")
+	ErrDomainerPanic          = errors.New("domain init panic")
+)
+
+type Rdser interface {
+	ListDetail(ctx context.Context, req *pbdomain.ListDetailReq) (resp *pbdomain.ListDetailResp, err error)
+}
+
+func NewDomainClient(provider pbtenant.CloudProvider, tenant tenanter.Tenanter) (rds Rdser, err error) {
+	// 部分sdk会在内部panic
+	defer func() {
+		if err1 := recover(); err1 != nil {
+			glog.Errorf("NewDomainClient panic %v", err1)
+			err = errors.WithMessagef(ErrDomainerPanic, "%v", err1)
+		}
+	}()
+
+	switch provider {
+	case pbtenant.CloudProvider_ali_cloud:
+		return NewAliDomainClient(tenant)
+	}
+
+	err = errors.WithMessagef(ErrDomainListNotSupported, "cloud provider %v ", provider)
+	return
+}
---
+++ internal/domainer/domainer_test.go
@@ -0,0 +1,66 @@
+package domainer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cloud-fitter/cloud-fitter/gen/idl/pbdomain"
+	"github.com/cloud-fitter/cloud-fitter/internal/tenanter"
+)
+
+func TestEcser_ListDetail(t *testing.T) {
+	ali, _ := NewAliDomainClient(aliTenant[0])
+	aliFailed, _ := NewAliDomainClient(tenanter.NewTenantWithAccessKey("empty", "", ""))
+
+	// region, _ = tenanter.NewRegion(pbtenant.CloudProvider_tencent_cloud, int32(pbtenant.TencentRegionId_tc_ap_beijing))
+	// tc, _ := NewTencentCdbClient(region, tcTenant[0])
+	// tcFailed, _ := NewTencentCdbClient(region, tenanter.NewTenantWithAccessKey("empty", "", ""))
+
+	// region, _ = tenanter.NewRegion(pbtenant.CloudProvider_huawei_cloud, int32(pbtenant.HuaweiRegionId_hw_cn_southwest_2))
+	// hw, _ := NewHuaweiEcsClient(region, hwTenant[0])
+	// // hwFailed, _ := NewHuaweiEcsClient(int32(pbtenant.HuaweiRegionId_hw_cn_north_1), tenanter.NewTenantWithAccessKey("empty", "", "", ""))
+	//
+	// region, _ = tenanter.NewRegion(pbtenant.CloudProvider_aws_cloud, int32(pbtenant.AwsRegionId_aws_us_east_2))
+	// aws, _ := NewAwsEcsClient(region, awsTenant[0])
+
+	// google, _ := NewGoogleEcsClient(tenanter.NewTenantWithAccessKey("", ""))
+
+	type args struct {
+		req *pbdomain.ListDetailReq
+	}
+	tests := []struct {
+		name    string
+		fields  Rdser
+		args    args
+		wantErr bool
+	}{
+		{name: "ali wrong cli", fields: aliFailed, args: args{&pbdomain.ListDetailReq{PageNumber: 1, PageSize: 1}}, wantErr: true},
+		{name: "ali right cli", fields: ali, args: args{&pbdomain.ListDetailReq{PageNumber: 1, PageSize: 10}}, wantErr: false},
+
+		// {name: "tc wrong cli", fields: tcFailed, args: args{&pbrds.ListDetailReq{PageNumber: 1, PageSize: 1}}, wantErr: true},
+		// {name: "tc wrong page number", fields: tc, args: args{&pbrds.ListDetailReq{PageNumber: 0, PageSize: 1}}, wantErr: true},
+		// {name: "tc wrong page size", fields: tc, args: args{&pbrds.ListDetailReq{PageNumber: 1, PageSize: 0}}, wantErr: true},
+		// {name: "tc right cli", fields: tc, args: args{&pbrds.ListDetailReq{PageNumber: 1, PageSize: 10}}, wantErr: false},
+
+		// {name: "hw wrong cli", fields: hwFailed, args: args{pageNumber: 1, pageSize: 1}, wantErr: true},
+		// {name: "hw right cli", fields: hw, args: args{&pbecs.ListDetailReq{PageNumber: 1, PageSize: 10}}, wantErr: false},
+
+		// {name: "aws right cli", fields: aws, args: args{&pbecs.ListDetailReq{PageNumber: 1, PageSize: 10}}, wantErr: false},
+
+		// {name: "right cli", fields: google, args: args{pageNumber: 1, pageSize: 10}, wantErr: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp, err := tt.fields.ListDetail(context.Background(), tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListDetail() error = %+v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			t.Logf("%+v", err)
+			if err == nil {
+				t.Log(resp)
+			}
+		})
+	}
+}
